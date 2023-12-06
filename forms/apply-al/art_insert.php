362b95f8c5c1f5f7675cc05445a2883331c5d820
nelitha13 <nelithavindinu7@gmail.com>
2023-12-06 09:39:38 +0530

    hdr ft

--- forms/apply-al/art_insert.php
+++ forms/apply-al/art_insert.php
@@ -1,38 +1,9 @@
 <html>
 
 <head>
-    <link href="css/bootstrap.css" rel="stylesheet">
-    <script type="text/javascript" src="js/jquery.min.js"></script>
-    <script type="text/javascript" src="js/bootstrap.js"></script>
-
-    <!-- Favicon -->
-    <link href="/content/icons/logo-70x70-pwc.png" rel="icon">
-    <link rel="icon" href="/content/icons/logo-70x70-pwc.ico" type="image/x-icon">
-    <link rel="apple-touch-icon" sizes="180x180" href="/content/icons/logo-apple-touch-icon-pwc.png">
-    <link rel="icon" type="image/png" sizes="192x192" href="/content/icons/logo-android-chrome-icon-pwc.png">
-    <meta name="msapplication-TileImage" content="/content/icons/logo-70x70-pwc.png">
-    <meta name="msapplication-TileColor" content="#800000">
-
-    <!-- Google Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600&family=Nunito:wght@600;700;800&display=swap"
-        rel="stylesheet">
-
-    <!-- Icon Font Stylesheet -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
-
-    <!-- Libraries Stylesheet -->
-    <link href="/resources/lib/animate/animate.min.css" rel="stylesheet">
-    <link href="/resources/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
-
-    <!-- Customized Bootstrap Stylesheet -->
-    <link href="/resources/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Stylesheet -->
-    <link href="/resources/css/style.css" rel="stylesheet">
+<?php
+include 'header.php';
+?>
 
 
     <style>
@@ -255,6 +226,11 @@ else{
 
 }
 
+?>
+
+
+<?php
+include 'footer.php';
 ?>
 
 </body>
