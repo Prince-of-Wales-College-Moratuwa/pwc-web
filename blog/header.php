09c10e0be5aba38daba3ba5350656e94b24bc157
nelitha13 <nelithavindinu7@gmail.com>
2024-01-02 10:38:24 +0530

    l

--- blog/header.php
+++ blog/header.php
@@ -114,7 +114,7 @@
 
     <!-- Navbar Start -->
     <nav class="navbar navbar-expand-lg bg-white navbar-light shadow sticky-top p-0">
-        <a href="../" class="navbar-brand d-flex align-items-center px-1 px-lg-4">
+        <a href="/" class="navbar-brand d-flex align-items-center px-1 px-lg-4">
             <img src="../../content/img/logo-pwc.png" alt="pwc logo" class="img-header">
             <h6 class="m-0 text-primary h6-header" style="font-family: 'Arimo', sans-serif; ">&nbsp; &nbsp; PRINCE OF WALES'
                 COLLEGE<br>&nbsp; &nbsp; MORATUWA
