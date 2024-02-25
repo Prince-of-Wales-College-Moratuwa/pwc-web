87860512ee55400992c1411c6c1b6291c4dbc283
nelitha13 <nelithavindinu7@gmail.com>
2024-02-25 15:30:28 +0530

    8n kiiyd

--- daily-period-log.php
+++ daily-period-log.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title>Daily Study Period Log</title>
+    <title>Project "8න් කීයද?"</title>
 
     <?php include 'header.php'; ?>
 
@@ -13,8 +13,8 @@
     <div class="container-xxl py-5">
         <div class="container">
             <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
-                <h6 class="section-title bg-white text-center text-primary px-3">DAILY</h6>
-                <h1 class="mb-5">PERIOD LOG</h1>
+                <h6 class="section-title bg-white text-center text-primary px-3">Project</h6>
+                <h1 class="mb-5">"8න් කීයද?"</h1>
             </div>
 
             <div class="container">
