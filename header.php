e48ad383b48ebfb486b37669b4b6b67eee46c472
nelitha13 <nelithavindinu7@gmail.com>
2024-03-24 14:52:45 +0530

    aaa

--- header.php
+++ header.php
@@ -21,8 +21,8 @@
         rel="stylesheet">
 
     <!-- Icon Font Stylesheet -->
-    <link href="/resources/css/all.min.css" rel="stylesheet">
-    <link href="/resources/css/bootstrap-icons.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
 
     <!-- Libraries Stylesheet -->
     <link href="/resources/lib/animate/animate.min.css" rel="stylesheet">
