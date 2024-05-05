a0aeea9afd99743ddc954e48cdd00eb6cc977a4e
nelitha13 <nelithavindinu7@gmail.com>
2024-05-05 11:19:01 +0530

    pattern

--- footer.php
+++ footer.php
@@ -1,3 +1,13 @@
+<style>
+    .footer {
+    background-image: url('/patterntp.webp');
+    background-size: cover;
+    background-position: center;
+    padding-top: 100px;
+}
+
+</style>
+
 <!-- Footer Start -->
 <div class="container-fluid bg-dark text-light footer pt-5 mt-5 wow fadeIn" data-wow-delay="0.1s">
     <div class="container py-5">
