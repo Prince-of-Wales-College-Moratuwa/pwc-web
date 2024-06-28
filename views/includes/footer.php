4a369991fe3ead313a9999d86f7c137be3a84e0e
nelitha13 <nelithavindinu7@gmail.com>
2024-06-28 15:43:43 +0530

    aaa

--- views/includes/footer.php
+++ views/includes/footer.php
@@ -126,11 +126,11 @@ window.addEventListener('scroll', () => {
 <script defer src="https://code.jquery.com/jquery-3.7.1.min.js"
     integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-<script defer src="https://princeofwales.edu.lk/resources/lib/wow/wow.min.js"></script>
-<script defer src="https://princeofwales.edu.lk/resources/lib/easing/easing.min.js"></script>
-<script defer src="https://princeofwales.edu.lk/resources/lib/waypoints/waypoints.min.js"></script>
-<script defer src="https://princeofwales.edu.lk/resources/lib/owlcarousel/owl.carousel.min.js"></script>
+<script defer src="/resources/lib/wow/wow.min.js"></script>
+<script defer src="/resources/lib/easing/easing.min.js"></script>
+<script defer src="/resources/lib/waypoints/waypoints.min.js"></script>
+<script defer src="/resources/lib/owlcarousel/owl.carousel.min.js"></script>
 
 <!-- Main JavaScript -->
 <!-- Defer the loading of your main JavaScript file -->
-<script defer src="https://princeofwales.edu.lk/resources/js/main.js"></script>
+<script defer src="/resources/js/main.js"></script>
