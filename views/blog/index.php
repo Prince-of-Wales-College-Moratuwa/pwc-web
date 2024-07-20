54c22b20fc74c1d4d91ab1e3754837b7ed1c4f68
nelitha13 <nelithavindinu7@gmail.com>
2024-07-20 13:00:40 +0530

    /blog

--- views/blog/index.php
+++ views/blog/index.php
@@ -109,7 +109,7 @@
                 ?>
                 <div class="col-md-4 animate-box wow fadeInUp ">
                     <article class="article-entry">
-                        <a href="<?php echo $row["slug"]; ?>" class="blog-img">
+                        <a href="/blog/<?php echo $row["slug"]; ?>" class="blog-img">
                             <img src="/content/img/img-blog/<?php echo $row["photo"]; ?>" alt="<?php echo $row["title"]; ?>" loading="lazy"><br><br>
                             <p class="meta"><span class="day"><?php echo $row["date"]; ?></span> │ <span></span>
                                 <span><?php echo $row["category"]; ?></span></p>
