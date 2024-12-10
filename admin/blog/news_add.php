54151c12994658607e2ba9926935a771fd10d2fe
nelitha13 <nelithavindinu7@gmail.com>
2024-12-10 12:56:14 +0530

    n

--- admin/blog/news_add.php
+++ admin/blog/news_add.php
@@ -251,9 +251,12 @@ if (isset($_POST['publish_telegram']) && $_POST['publish_telegram'] == 'on') {
 	include '../auto-post/tg_auto.php';
 }
 
-if (isset($_POST['publish_facebook']) && $_POST['publish_facebook'] == 'on') {
-	include '../auto-post/fb_auto.php';
+if (isset($_POST['publish_facebook']) && $_POST['publish_facebook'] == 'on' && $_POST['date'] <= date('Y-m-d H:i:s')) {
+    include '../auto-post/fb_auto.php';
+} else {
+    include '../auto-post/fb_schedule.php';
 }
+
 
 echo '<script>window.open("/blog/' . $formdata['slug'] . '", "_blank");</script>';
 exit();
