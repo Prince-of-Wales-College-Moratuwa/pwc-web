cfd4fe07804405a8ee35b075978d75785881c75c
nelitha13 <nelithavindinu7@gmail.com>
2024-12-11 08:48:52 +0530

    a

--- rss.php
+++ rss.php
@@ -31,29 +31,7 @@ echo '<?xml version="1.0" encoding="UTF-8" ?>';
     <item>
       <title><?php echo htmlspecialchars($row["title"], ENT_QUOTES, 'UTF-8'); ?></title>
       <link>https://princeofwales.edu.lk/blog/<?php echo htmlspecialchars($row["slug"], ENT_QUOTES, 'UTF-8'); ?></link>
-
-      <description>
-      <?php 
-  $content = html_entity_decode($row["content"]);
-  
-  // Convert HTML tags to Markdown
-  $content = preg_replace('/<strong>(.*?)<\/strong>/', '**$1**', $content);   // <strong> to ** for bold
-  $content = preg_replace('/<em>(.*?)<\/em>/', '*$1*', $content);           // <em> to * for italics
-  $content = preg_replace('/<i>(.*?)<\/i>/', '*$1*', $content);             // <i> to * for italics
-  $content = preg_replace('/<b>(.*?)<\/b>/', '**$1**', $content);           // <b> to ** for bold
-  $content = preg_replace('/<u>(.*?)<\/u>/', '<u>$1</u>', $content);         // <u> is rare, preserving as-is
-  $content = preg_replace('/<a href="(.*?)">(.*?)<\/a>/', '[$2]($1)', $content); // <a> to [text](url) for links
-  $content = preg_replace('/<p>(.*?)<\/p>/', "\n\n$1", $content);           // <p> to new lines
-  $content = preg_replace('/<br\s*\/?>/', "\n", $content);                  // <br> to new lines
-  
-  // Output the content with newlines
-  echo $content; 
-?>
-
-</description>
-
-
-
+      <description><?php echo htmlspecialchars($row["content"], ENT_QUOTES, 'UTF-8'); ?></description>
       <author><?php echo htmlspecialchars($row["author"], ENT_QUOTES, 'UTF-8'); ?></author>
       <pubDate><?php echo date(DATE_RSS, strtotime($row["date"])); ?></pubDate>
       <guid>https://princeofwales.edu.lk/blog/<?php echo htmlspecialchars($row["slug"], ENT_QUOTES, 'UTF-8'); ?></guid>
@@ -61,7 +39,8 @@ echo '<?xml version="1.0" encoding="UTF-8" ?>';
       <?php if (!empty($row["photo"])): ?>
         <enclosure 
           url="https://princeofwales.edu.lk/content/img/img-blog/<?php echo htmlspecialchars($row["photo"], ENT_QUOTES, 'UTF-8'); ?>" 
-          type="image/webp"  />
+          type="image/jpeg" 
+          length="<?php echo filesize("../content/img/img-blog/" . $row["photo"]); ?>" />
       <?php endif; ?>
 
     </item>
