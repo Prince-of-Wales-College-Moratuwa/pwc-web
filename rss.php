659589cba45edabf3d106d7943df106d4e4485cd
nelitha13 <nelithavindinu7@gmail.com>
2025-03-08 21:13:56 +0530

    Add photo size to RSS feed enclosure for blog images

--- rss.php
+++ rss.php
@@ -35,9 +35,18 @@ echo '<?xml version="1.0" encoding="UTF-8" ?>';
       <guid>https://princeofwales.edu.lk/blog/<?php echo htmlspecialchars($row["slug"], ENT_QUOTES, 'UTF-8'); ?></guid>
 
       <?php if (!empty($row["photo"])): ?>
+        <?php
+          $photoPath = "../content/img/img-blog/" . $row["photo"];
+          if (file_exists($photoPath)) {
+              $photoSize = filesize($photoPath); 
+          } else {
+              $photoSize = 1024; 
+          }
+        ?>
         <enclosure 
           url="https://princeofwales.edu.lk/content/img/img-blog/<?php echo htmlspecialchars($row["photo"], ENT_QUOTES, 'UTF-8'); ?>" 
-          type="image/webp" />
+          type="image/webp"
+          length="<?php echo $photoSize; ?>" />
       <?php endif; ?>
 
       <?php if (!empty($row["extra_link"])): ?>
