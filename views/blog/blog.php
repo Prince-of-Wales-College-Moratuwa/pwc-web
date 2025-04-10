06831e9baf088410bebc6fedcc50c1a0957f39c3
nelitha13 <nelithavindinu7@gmail.com>
2025-04-10 10:37:13 +0530

    Refactor blog.php for improved readability and consistency in code formatting

--- views/blog/blog.php
+++ views/blog/blog.php
@@ -3,27 +3,27 @@
 
 <head>
   <?php include '../../database_connection.php';
-    $page = 'blog';
-  
+  $page = 'blog';
+
   ?>
 
   <?php
 
 
 
-$slug = $_GET['slug'];
-$query = "SELECT * FROM pwc_db_news WHERE slug='$slug'";
-$statement = $connect->prepare($query);
-$statement->execute();
-$rows = $statement->fetchAll();
+  $slug = $_GET['slug'];
+  $query = "SELECT * FROM pwc_db_news WHERE slug='$slug'";
+  $statement = $connect->prepare($query);
+  $statement->execute();
+  $rows = $statement->fetchAll();
 
-if (count($rows) === 0) {
+  if (count($rows) === 0) {
     header("Location: https://princeofwales.edu.lk/404");
-}
+  }
 
-foreach ($rows as $row) {
-}
-?>
+  foreach ($rows as $row) {
+  }
+  ?>
 
   <title><?php echo $row["title"]; ?></title>
 
@@ -68,8 +68,8 @@ foreach ($rows as $row) {
   <meta name="distribution" content="global">
 
   <?php include '../includes/header.php';
-    include '../includes/greetings.php';
-  
+  include '../includes/greetings.php';
+
   ?>
 
 
@@ -132,14 +132,14 @@ foreach ($rows as $row) {
 
     <?php
 
-$schoolPride = $row["schoolPride"];  
+    $schoolPride = $row["schoolPride"];
 
-if ($schoolPride == 'ON') {
-    echo '<div class="confetti-container"></div>';
-    
-    echo '<script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.9.2/dist/confetti.browser.min.js"></script>';
-    
-    echo '
+    if ($schoolPride == 'ON') {
+      echo '<div class="confetti-container"></div>';
+
+      echo '<script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.9.2/dist/confetti.browser.min.js"></script>';
+
+      echo '
     <script>
       var end = Date.now() + (2 * 1000);
       var colors = [\'#800080\', \'#ffd700\', \'#800000\'];
@@ -165,20 +165,20 @@ if ($schoolPride == 'ON') {
         }
       }());
     </script>';
-}
+    }
 
-?>
+    ?>
 
 
     <script>
       function openFacebookLinksInNewTab() {
         var links = document.querySelectorAll('a[href*="facebook.com"]');
-        links.forEach(function (link) {
+        links.forEach(function(link) {
           link.setAttribute('target', '_blank');
         });
       }
 
-      document.addEventListener('DOMContentLoaded', function () {
+      document.addEventListener('DOMContentLoaded', function() {
         openFacebookLinksInNewTab();
       });
     </script>
@@ -196,13 +196,16 @@ if ($schoolPride == 'ON') {
                 <h6 class="section-title bg-white text-start text-primary pe-3"><?php echo $row["category"]; ?></h6>
               </a>
               <h3 class="mb-4"><?php echo $row["title"]; ?></h3>
-              <h4 class="h6 bg-white text-start text-primary" style="font-size: 15px;"><svg
-                  xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                  class="bi bi-patch-check-fill" viewBox="0 0 16 16">
-                  <path
-                    d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01-.622-.636zm.287 5.984-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708z" />
-                </svg> Published By: <?php echo $row["author"]; ?> |
-                <?php $date = $row["date"]; echo date("Y-m-d h:i A", strtotime($date));  ?></h4>
+              <h4 style="vertical-align: middle;" class="h6 bg-white text-start text-primary">
+                Published By: <?php echo $row["author"]; ?>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                  class="bi bi-patch-check-fill" viewBox="0 0 16 16"
+                  style="vertical-align: TOP;">
+                  <path d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01-.622-.636zm.287 5.984-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708z" />
+                </svg>
+                | <?php $date = $row["date"];
+                  echo date("Y-m-d h:i A", strtotime($date)); ?>
+              </h4>
               <br>
               <div class="position-relative">
                 <img class="img-fluid w-100 h-100" src="/content/img/img-blog/<?php echo $row["photo"]; ?>"
@@ -210,18 +213,18 @@ if ($schoolPride == 'ON') {
                 <br><br>
               </div>
               <p class="mb-4"><?php echo $row["content"]; ?></p>
-              <?php 
-if (!empty($row["extra_link"])): 
-    $parts = explode(" | ", $row["extra_link"]); // Split text and URL
-    if (count($parts) == 2): 
-?>
-              <p class="mb-4"><?php echo htmlspecialchars($parts[0]); ?> | <a
-                  href="<?php echo htmlspecialchars($parts[1]); ?>"
-                  target="_blank"><?php echo htmlspecialchars($parts[1]); ?></a></p>
-              <?php 
-    endif;
-endif; 
-?>
+              <?php
+              if (!empty($row["extra_link"])):
+                $parts = explode(" | ", $row["extra_link"]); // Split text and URL
+                if (count($parts) == 2):
+              ?>
+                  <p class="mb-4"><?php echo htmlspecialchars($parts[0]); ?> | <a
+                      href="<?php echo htmlspecialchars($parts[1]); ?>"
+                      target="_blank"><?php echo htmlspecialchars($parts[1]); ?></a></p>
+              <?php
+                endif;
+              endif;
+              ?>
               <p class="mb-4"><?php echo $row["tags"]; ?></p>
             </div>
           </div>
@@ -237,25 +240,31 @@ endif;
                 <h6 class="section-title bg-white text-start text-primary pe-3"><?php echo $row["category"]; ?></h6>
               </a>
               <h3 class="mb-4"><?php echo $row["title"]; ?></h3>
-              <h4 class="h6 bg-white text-start text-primary"><svg xmlns="http://www.w3.org/2000/svg" width="16"
-                  height="16" fill="currentColor" class="bi bi-patch-check-fill" viewBox="0 0 16 16">
-                  <path
-                    d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01-.622-.636zm.287 5.984-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708z" />
-                </svg> Published By: <?php echo $row["author"]; ?> |
-                <?php $date = $row["date"]; echo date("Y-m-d h:i A", strtotime($date));  ?></h4>
+
+              <h4 style="vertical-align: middle;" class="h6 bg-white text-start text-primary">
+                Published By: <?php echo $row["author"]; ?>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                  class="bi bi-patch-check-fill" viewBox="0 0 16 16"
+                  style="vertical-align: TOP;">
+                  <path d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01-.622-.636zm.287 5.984-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708z" />
+                </svg>
+                | <?php $date = $row["date"];
+                  echo date("Y-m-d h:i A", strtotime($date)); ?>
+              </h4>
+
               <p class="mb-4"><?php echo $row["content"]; ?></p>
-              <?php 
-if (!empty($row["extra_link"])): 
-    $parts = explode(" | ", $row["extra_link"]); // Split text and URL
-    if (count($parts) == 2): 
-?>
-              <p class="mb-4"><?php echo htmlspecialchars($parts[0]); ?> | <a
-                  href="<?php echo htmlspecialchars($parts[1]); ?>"
-                  target="_blank"><?php echo htmlspecialchars($parts[1]); ?></a></p>
-              <?php 
-    endif;
-endif; 
-?>
+              <?php
+              if (!empty($row["extra_link"])):
+                $parts = explode(" | ", $row["extra_link"]); // Split text and URL
+                if (count($parts) == 2):
+              ?>
+                  <p class="mb-4"><?php echo htmlspecialchars($parts[0]); ?> | <a
+                      href="<?php echo htmlspecialchars($parts[1]); ?>"
+                      target="_blank"><?php echo htmlspecialchars($parts[1]); ?></a></p>
+              <?php
+                endif;
+              endif;
+              ?>
               <p class="mb-4"><?php echo $row["tags"]; ?></p>
             </div>
             <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.3s">
